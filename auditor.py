140fb4c1933f4fe78d406019d0045233b7b1fc56
MoeHan-lab <2605699@sit.singaporetech.edu.sg>
2026-09-10 20:27:12 +0800

    Add while loop

--- auditor.py
+++ auditor.py
@@ -1,2 +1,9 @@
 print("=== Inventory Audit System ===")
+print("Enter stock quantity for each delivery.")
+print("Type 'quit' at any time to stop and see the report.\n")
+while True:
+        user_input = input("Enter stock quantity: ")
+
+        if user_input.lower() == "quit":
+            break
 total_inventory = 0 
