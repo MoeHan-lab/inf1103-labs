770450bf412086e46e9a350f989370b3d28780b6
MoeHan-lab <2605699@sit.singaporetech.edu.sg>
2026-09-24 14:34:06 +0800

    add back week2

---
+++ week2/app.py
@@ -0,0 +1,22 @@
+print("================================")
+print("Welcome here")
+print("My first post!")
+print("================================")
+
+username = "cool_creator"
+bio = "Fun blogger"
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+print("username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
